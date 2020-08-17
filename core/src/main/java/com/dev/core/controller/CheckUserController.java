ada936ba68e8996822057bc9c93cd2d2e3b51ced
MaXiao <813611810@qq.com>
2020-08-17 15:52:58 +0800

    新增请求拦截验证token

---
+++ core/src/main/java/com/dev/core/controller/CheckUserController.java
@@ -0,0 +1,34 @@
+package com.dev.core.controller;
+
+import com.dev.core.util.RedisUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/core")
+public class CheckUserController {
+
+    @Resource
+    private RedisUtil redisUtil;
+    /**
+     * 查询用户token是否存在
+     *
+     * @author max
+     * @create 2020/8/17
+     **/
+    @GetMapping("/testGet")
+    public String CheckToken(String uid) {
+        Object token = redisUtil.get(uid);
+        if(token==null){
+            return null;
+        }else{
+            //同时延长该token生命周期  30min
+            redisUtil.expire(uid,1800);
+            return token.toString();
+        }
+    }
+
+}
